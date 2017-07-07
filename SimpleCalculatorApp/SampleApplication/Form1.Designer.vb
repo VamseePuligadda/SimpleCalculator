7fbcbf4eadeeed42ecc0c81b7d05b660f98a7012
Vamsee Puligadda <vamsee.puligadda@gmail.com>
2017-07-07 12:55:48 +0530

    Add files via upload

---
+++ SimpleCalculatorApp/SampleApplication/Form1.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Monitor = New System.Windows.Forms.TextBox()
+        Me.Switch7 = New System.Windows.Forms.Button()
+        Me.Switch9 = New System.Windows.Forms.Button()
+        Me.Switch8 = New System.Windows.Forms.Button()
+        Me.Switch6 = New System.Windows.Forms.Button()
+        Me.Switch5 = New System.Windows.Forms.Button()
+        Me.Switch4 = New System.Windows.Forms.Button()
+        Me.Switch2 = New System.Windows.Forms.Button()
+        Me.Switch3 = New System.Windows.Forms.Button()
+        Me.Switch1 = New System.Windows.Forms.Button()
+        Me.Switch0 = New System.Windows.Forms.Button()
+        Me.SwitchDot = New System.Windows.Forms.Button()
+        Me.SwitchMinus = New System.Windows.Forms.Button()
+        Me.SwitchPlus = New System.Windows.Forms.Button()
+        Me.SwitchMultipliedBy = New System.Windows.Forms.Button()
+        Me.SwitchEqualsTo = New System.Windows.Forms.Button()
+        Me.SwitchDividedBy = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Monitor
+        '
+        Me.Monitor.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Monitor.Location = New System.Drawing.Point(0, 3)
+        Me.Monitor.Multiline = True
+        Me.Monitor.Name = "Monitor"
+        Me.Monitor.ReadOnly = True
+        Me.Monitor.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Monitor.Size = New System.Drawing.Size(146, 33)
+        Me.Monitor.TabIndex = 0
+        Me.Monitor.Text = "0"
+        '
+        'Switch7
+        '
+        Me.Switch7.Location = New System.Drawing.Point(0, 58)
+        Me.Switch7.Name = "Switch7"
+        Me.Switch7.Size = New System.Drawing.Size(32, 23)
+        Me.Switch7.TabIndex = 1
+        Me.Switch7.Text = "7"
+        Me.Switch7.UseVisualStyleBackColor = True
+        '
+        'Switch9
+        '
+        Me.Switch9.Location = New System.Drawing.Point(76, 58)
+        Me.Switch9.Name = "Switch9"
+        Me.Switch9.Size = New System.Drawing.Size(32, 23)
+        Me.Switch9.TabIndex = 2
+        Me.Switch9.Text = "9"
+        Me.Switch9.UseVisualStyleBackColor = True
+        '
+        'Switch8
+        '
+        Me.Switch8.Location = New System.Drawing.Point(38, 58)
+        Me.Switch8.Name = "Switch8"
+        Me.Switch8.Size = New System.Drawing.Size(32, 23)
+        Me.Switch8.TabIndex = 3
+        Me.Switch8.Text = "8"
+        Me.Switch8.UseVisualStyleBackColor = True
+        '
+        'Switch6
+        '
+        Me.Switch6.Location = New System.Drawing.Point(77, 92)
+        Me.Switch6.Name = "Switch6"
+        Me.Switch6.Size = New System.Drawing.Size(32, 23)
+        Me.Switch6.TabIndex = 4
+        Me.Switch6.Text = "6"
+        Me.Switch6.UseVisualStyleBackColor = True
+        '
+        'Switch5
+        '
+        Me.Switch5.Location = New System.Drawing.Point(38, 92)
+        Me.Switch5.Name = "Switch5"
+        Me.Switch5.Size = New System.Drawing.Size(32, 23)
+        Me.Switch5.TabIndex = 5
+        Me.Switch5.Text = "5"
+        Me.Switch5.UseVisualStyleBackColor = True
+        '
+        'Switch4
+        '
+        Me.Switch4.Location = New System.Drawing.Point(0, 92)
+        Me.Switch4.Name = "Switch4"
+        Me.Switch4.Size = New System.Drawing.Size(32, 23)
+        Me.Switch4.TabIndex = 6
+        Me.Switch4.Text = "4"
+        Me.Switch4.UseVisualStyleBackColor = True
+        '
+        'Switch2
+        '
+        Me.Switch2.Location = New System.Drawing.Point(38, 126)
+        Me.Switch2.Name = "Switch2"
+        Me.Switch2.Size = New System.Drawing.Size(32, 23)
+        Me.Switch2.TabIndex = 7
+        Me.Switch2.Text = "2"
+        Me.Switch2.UseVisualStyleBackColor = True
+        '
+        'Switch3
+        '
+        Me.Switch3.Location = New System.Drawing.Point(77, 126)
+        Me.Switch3.Name = "Switch3"
+        Me.Switch3.Size = New System.Drawing.Size(32, 23)
+        Me.Switch3.TabIndex = 8
+        Me.Switch3.Text = "3"
+        Me.Switch3.UseVisualStyleBackColor = True
+        '
+        'Switch1
+        '
+        Me.Switch1.Location = New System.Drawing.Point(0, 127)
+        Me.Switch1.Name = "Switch1"
+        Me.Switch1.Size = New System.Drawing.Size(32, 23)
+        Me.Switch1.TabIndex = 9
+        Me.Switch1.Text = "1"
+        Me.Switch1.UseVisualStyleBackColor = True
+        '
+        'Switch0
+        '
+        Me.Switch0.Location = New System.Drawing.Point(39, 158)
+        Me.Switch0.Name = "Switch0"
+        Me.Switch0.Size = New System.Drawing.Size(32, 23)
+        Me.Switch0.TabIndex = 10
+        Me.Switch0.Text = "0"
+        Me.Switch0.UseVisualStyleBackColor = True
+        '
+        'SwitchDot
+        '
+        Me.SwitchDot.Location = New System.Drawing.Point(0, 159)
+        Me.SwitchDot.Name = "SwitchDot"
+        Me.SwitchDot.Size = New System.Drawing.Size(32, 23)
+        Me.SwitchDot.TabIndex = 11
+        Me.SwitchDot.Text = "C"
+        Me.SwitchDot.UseVisualStyleBackColor = True
+        '
+        'SwitchMinus
+        '
+        Me.SwitchMinus.Location = New System.Drawing.Point(115, 92)
+        Me.SwitchMinus.Name = "SwitchMinus"
+        Me.SwitchMinus.Size = New System.Drawing.Size(32, 23)
+        Me.SwitchMinus.TabIndex = 12
+        Me.SwitchMinus.Text = "-"
+        Me.SwitchMinus.UseVisualStyleBackColor = True
+        '
+        'SwitchPlus
+        '
+        Me.SwitchPlus.Location = New System.Drawing.Point(114, 58)
+        Me.SwitchPlus.Name = "SwitchPlus"
+        Me.SwitchPlus.Size = New System.Drawing.Size(32, 23)
+        Me.SwitchPlus.TabIndex = 13
+        Me.SwitchPlus.Text = "+"
+        Me.SwitchPlus.UseVisualStyleBackColor = True
+        '
+        'SwitchMultipliedBy
+        '
+        Me.SwitchMultipliedBy.Location = New System.Drawing.Point(114, 127)
+        Me.SwitchMultipliedBy.Name = "SwitchMultipliedBy"
+        Me.SwitchMultipliedBy.Size = New System.Drawing.Size(32, 23)
+        Me.SwitchMultipliedBy.TabIndex = 14
+        Me.SwitchMultipliedBy.Text = "*"
+        Me.SwitchMultipliedBy.UseVisualStyleBackColor = True
+        '
+        'SwitchEqualsTo
+        '
+        Me.SwitchEqualsTo.Location = New System.Drawing.Point(77, 157)
+        Me.SwitchEqualsTo.Name = "SwitchEqualsTo"
+        Me.SwitchEqualsTo.Size = New System.Drawing.Size(32, 23)
+        Me.SwitchEqualsTo.TabIndex = 15
+        Me.SwitchEqualsTo.Text = "="
+        Me.SwitchEqualsTo.UseVisualStyleBackColor = True
+        '
+        'SwitchDividedBy
+        '
+        Me.SwitchDividedBy.Location = New System.Drawing.Point(114, 156)
+        Me.SwitchDividedBy.Name = "SwitchDividedBy"
+        Me.SwitchDividedBy.Size = New System.Drawing.Size(32, 23)
+        Me.SwitchDividedBy.TabIndex = 16
+        Me.SwitchDividedBy.Text = "/"
+        Me.SwitchDividedBy.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(146, 193)
+        Me.Controls.Add(Me.SwitchDividedBy)
+        Me.Controls.Add(Me.SwitchEqualsTo)
+        Me.Controls.Add(Me.SwitchMultipliedBy)
+        Me.Controls.Add(Me.SwitchPlus)
+        Me.Controls.Add(Me.SwitchMinus)
+        Me.Controls.Add(Me.SwitchDot)
+        Me.Controls.Add(Me.Switch0)
+        Me.Controls.Add(Me.Switch1)
+        Me.Controls.Add(Me.Switch3)
+        Me.Controls.Add(Me.Switch2)
+        Me.Controls.Add(Me.Switch4)
+        Me.Controls.Add(Me.Switch5)
+        Me.Controls.Add(Me.Switch6)
+        Me.Controls.Add(Me.Switch8)
+        Me.Controls.Add(Me.Switch9)
+        Me.Controls.Add(Me.Switch7)
+        Me.Controls.Add(Me.Monitor)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Monitor As System.Windows.Forms.TextBox
+    Friend WithEvents Switch7 As System.Windows.Forms.Button
+    Friend WithEvents Switch9 As System.Windows.Forms.Button
+    Friend WithEvents Switch8 As System.Windows.Forms.Button
+    Friend WithEvents Switch6 As System.Windows.Forms.Button
+    Friend WithEvents Switch5 As System.Windows.Forms.Button
+    Friend WithEvents Switch4 As System.Windows.Forms.Button
+    Friend WithEvents Switch2 As System.Windows.Forms.Button
+    Friend WithEvents Switch3 As System.Windows.Forms.Button
+    Friend WithEvents Switch1 As System.Windows.Forms.Button
+    Friend WithEvents Switch0 As System.Windows.Forms.Button
+    Friend WithEvents SwitchDot As System.Windows.Forms.Button
+    Friend WithEvents SwitchMinus As System.Windows.Forms.Button
+    Friend WithEvents SwitchPlus As System.Windows.Forms.Button
+    Friend WithEvents SwitchMultipliedBy As System.Windows.Forms.Button
+    Friend WithEvents SwitchEqualsTo As System.Windows.Forms.Button
+    Friend WithEvents SwitchDividedBy As System.Windows.Forms.Button
+
+End Class
---
+++ SimpleCalculatorApp/SampleApplication/Form1.vb
@@ -0,0 +1,161 @@
+﻿Imports System.Linq.Expressions
+
+Public Class Form1
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Switch8.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "8"
+            Else
+                Monitor.Text = Monitor.Text + "8"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "8"
+        End If
+    End Sub
+
+    Private Sub Switch0_Click(sender As Object, e As EventArgs) Handles Switch0.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "0"
+            Else
+                Monitor.Text = Monitor.Text + "0"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "0"
+        End If
+
+    End Sub
+
+    Private Sub Switch1_Click(sender As Object, e As EventArgs) Handles Switch1.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "1"
+            Else
+                Monitor.Text = Monitor.Text + "1"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "1"
+        End If
+    End Sub
+
+    Private Sub Switch2_Click(sender As Object, e As EventArgs) Handles Switch2.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "2"
+            Else
+                Monitor.Text = Monitor.Text + "2"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "2"
+        End If
+    End Sub
+
+    Private Sub Switch3_Click(sender As Object, e As EventArgs) Handles Switch3.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "3"
+            Else
+                Monitor.Text = Monitor.Text + "3"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "3"
+        End If
+    End Sub
+
+    Private Sub Switch4_Click(sender As Object, e As EventArgs) Handles Switch4.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "4"
+            Else
+                Monitor.Text = Monitor.Text + "4"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "4"
+        End If
+    End Sub
+
+    Private Sub Switch5_Click(sender As Object, e As EventArgs) Handles Switch5.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "5"
+            Else
+                Monitor.Text = Monitor.Text + "5"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "5"
+        End If
+    End Sub
+
+    Private Sub Switch6_Click(sender As Object, e As EventArgs) Handles Switch6.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "6"
+            Else
+                Monitor.Text = Monitor.Text + "6"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "6"
+        End If
+    End Sub
+
+    Private Sub Switch7_Click(sender As Object, e As EventArgs) Handles Switch7.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "7"
+            Else
+                Monitor.Text = Monitor.Text + "7"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "7"
+        End If
+    End Sub
+
+    Private Sub Switch9_Click(sender As Object, e As EventArgs) Handles Switch9.Click
+        Dim number As Integer
+        If Integer.TryParse(Monitor.Text, number) Then
+            If Integer.Parse(Monitor.Text) = 0 Then
+                Monitor.Text = "9"
+            Else
+                Monitor.Text = Monitor.Text + "9"
+            End If
+        Else
+            Monitor.Text = Monitor.Text + "9"
+        End If
+    End Sub
+
+    Private Sub SwitchDot_Click(sender As Object, e As EventArgs) Handles SwitchDot.Click
+        Monitor.Text = "0"
+    End Sub
+
+    Private Sub SwitchEqualsTo_Click(sender As Object, e As EventArgs) Handles SwitchEqualsTo.Click
+        Dim TextOnScreen As String = Monitor.Text
+        Dim result = New DataTable().Compute(TextOnScreen, Nothing)
+        Monitor.Text = result
+    End Sub
+
+    Private Sub SwitchDividedBy_Click(sender As Object, e As EventArgs) Handles SwitchDividedBy.Click
+        Monitor.Text = Monitor.Text + "/"
+    End Sub
+
+    Private Sub SwitchMultipliedBy_Click(sender As Object, e As EventArgs) Handles SwitchMultipliedBy.Click
+        Monitor.Text = Monitor.Text + "*"
+    End Sub
+
+    Private Sub SwitchMinus_Click(sender As Object, e As EventArgs) Handles SwitchMinus.Click
+        Monitor.Text = Monitor.Text + "-"
+    End Sub
+
+    Private Sub SwitchPlus_Click(sender As Object, e As EventArgs) Handles SwitchPlus.Click
+        Monitor.Text = Monitor.Text + "+"
+    End Sub
+End Class
